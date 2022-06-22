0b646c9d7582ca320c47fb024e48bdc64f2dc677
Kapil Suri <kapil.suri.au@outlook.com>
2022-06-22 16:36:31 +1000

    Added EF Core and Scaffolded Database

---
+++ BookStoreApp.API/Data/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStoreApp.API.Data
+{
+    public partial class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Bio { get; set; } = null!;
+    }
+}
---
+++ BookStoreApp.API/Data/BookStoreContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BookStoreApp.API.Data
+{
+    public partial class BookStoreContext : DbContext
+    {
+        public BookStoreContext()
+        {
+        }
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Bio).HasMaxLength(500);
+
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+
+                entity.Property(e => e.LastName).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.Isbn, "IX_Books_ISBN")
+                    .IsUnique();
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Isbn)
+                    .HasMaxLength(50)
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Summary).HasMaxLength(250);
+
+                entity.Property(e => e.Title).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
